c1874e254e12f5284b1ab40d5cad799472e2dc5e
Travis Lee <travisslee223@gmail.com>
2020-10-20 17:34:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,7 @@ class App < Sinatra::Base
   end
   
   get "/multiply/:num1/:num2" do
-    @num1 = params[:num1].to_i 
-    @num2 = params[:num2].to_i 
-    (num1 * num2).to_s
+     @result = params[:num1].to_i*params[:num2].to_i
+    "#{@result}"
   end
 end
